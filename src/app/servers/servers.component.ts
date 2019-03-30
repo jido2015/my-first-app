c483b15370a90dd2e5ed6c579e43cb937adfae79
Bashorun Olajide <bashorun.olajide@gmail.com>
2019-03-30 08:38:36 +0100

    Use two ways data binging

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,7 +9,7 @@ export class ServersComponent implements OnInit {
 
   allowNewServer = false;
   serverCreationStatus = 'No server created';
-  serverName = '';
+  serverName = 'Testserver';
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
