2f2687ca85a7722e9e1d2864c749ae332436e767
Bashorun Olajide <bashorun.olajide@gmail.com>
2019-03-31 13:24:24 +0100

    Assignment 2

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {stringify} from 'querystring';
+import {el} from '@angular/platform-browser/testing/src/browser_util';
 
 @Component({
   selector: 'app-servers',
@@ -7,23 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 
-  allowNewServer = false;
-  serverCreationStatus = 'No server created';
-  serverName = 'Testserver';
+ // allowNewServer = false;
+ // serverCreationStatus = 'No server created';
+ // serverName = 'Testserver';
+
+  username = '';
+  disableButton = false;
   constructor() {
-    setTimeout(() => {
+/*    setTimeout(() => {
       this.allowNewServer = true;
-    }, 2000);
+    }, 2000);*/
+
   }
 
   ngOnInit() {
   }
 
-  onCreateServer() {
+ /* onCreateServer() {
     this.serverCreationStatus = 'Server was created ' + this.serverName;
-  }
+  }*/
 
   onUpdateServerName(event: Event) {
-    this.serverName = (<HTMLInputElement> event.target).value;
+  }
+
+  checkUsername(event: Event) {
+    this.username = (<HTMLInputElement> event.target).value;
+    if (this.username.length > 0) {
+      this.disableButton = true;
+    } else {
+      this.disableButton = false;
+    }
+  }
+
+  resetInputButton() {
+    this.username = '';
+    this.disableButton = false;
   }
 }
