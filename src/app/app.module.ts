aadc21ce6964b553f1c51a267878de676d65eb7b
Bashorun Olajide <bashorun.olajide@gmail.com>
2019-04-04 18:04:14 +0100

    Course project: Shopping List. -  Adding a Navigation Bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {ServerComponent} from '../server/server.component';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
-import { ServersComponent } from './servers/servers.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './header/recipes/recipes.component';
+import { ShoppingListComponent } from './header/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './header/shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeDetailsComponent } from './header/recipes/recipe-details/recipe-details.component';
+import { RecipeListComponent } from './header/recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './header/recipes/recipe-list/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent,
+    HeaderComponent,
+    RecipesComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    RecipeDetailsComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+
   ],
   imports: [
     BrowserModule,
